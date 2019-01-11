6facebf5fda2f85c4d6b82e0b842cd5447ad3727
zhenglz <zhenglz@users.noreply.github.com>
2019-01-11 22:38:26 +0800

    modify the simple backup script

--- keepAlive/keep_backup.py
+++ keepAlive/keep_backup.py
@@ -1,23 +1,36 @@
-from .keep_alive import KeepAlive
+from keep_alive import KeepAlive
 import time
 import os
+#import psutil
 
-if __name__ == "__main__":
+def main():
 
     ka = KeepAlive()
     ka.params = ka.read_params("./parameters.dat")
+	
+    print("Backup now")
 
     while True:
         gamefolder = "dontstarve"
         bkp_loc = os.path.join(ka.params["BACKUP_LOCATION"], gamefolder)
 
         if os.path.isdir(bkp_loc):
-            if os.path.isdir(os.path.join(bkp_loc, "_prev")):
-                ka.delete_backups(os.path.join(bkp_loc, "_prev"))
+            prev = os.path.join(ka.params["BACKUP_LOCATION"], gamefolder+"_prev")
+            if os.path.isdir(prev):
+                ka.delete_backups(prev)
+                time.sleep(5)
+                print("Backup/%s_prev exists, delete it."%gamefolder)
 
-            ka.copy_files(bkp_loc, os.path.join(bkp_loc, "_prev"))
+            ka.copy_files(bkp_loc, prev)
+            time.sleep(5)
             ka.delete_backups(bkp_loc)
-
+            time.sleep(5)
+            
+        print("Working: backup files now... ")
         ka.copy_files(ka.params["ORIGIN_LOCATION"], bkp_loc)
 
+        print("Sleep here ... ")
         time.sleep(ka.params["TIME_GAP"])
+
+main()
+
